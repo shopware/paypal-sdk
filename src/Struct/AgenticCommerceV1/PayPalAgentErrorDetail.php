210edfb2359c6fb68ddf174b424e266fbd7ac8bf
Lennart Tinkloh <lennart@tinkloh.eu>
2025-07-08 14:01:30 +0200

    feat: move namespace

--- src/Struct/AgenticCommerceV1/PayPalAgentErrorDetail.php
+++ src/Struct/AgenticCommerceV1/PayPalAgentErrorDetail.php
@@ -5,7 +5,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Shopware\PayPalSDK\Struct\Error;
+namespace Shopware\PayPalSDK\Struct\AgenticCommerceV1;
 
 use Shopware\PayPalSDK\Struct\Struct;
 
