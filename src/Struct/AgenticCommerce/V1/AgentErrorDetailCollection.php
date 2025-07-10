05fef64a02d823ba388b186382a39f8a0d1588ca
Lennart Tinkloh <21331033+lernhart@users.noreply.github.com>
2025-07-10 12:53:32 +0200

    fix: error structs namespaces (#39)

--- src/Struct/AgenticCommerce/V1/AgentErrorDetailCollection.php
+++ src/Struct/AgenticCommerce/V1/AgentErrorDetailCollection.php
@@ -5,7 +5,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Shopware\PayPalSDK\Struct\AgenticCommerceV1;
+namespace Shopware\PayPalSDK\Struct\AgenticCommerce\V1;
 
 use Shopware\PayPalSDK\Struct\Collection;
 
