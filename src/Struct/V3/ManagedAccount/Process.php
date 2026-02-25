9d39c9ce27272be0b654b0f597f5edc0e123b9ab
Lennart Tinkloh <l.tinkloh@shopware.com>
2026-02-25 14:49:15 +0100

    feat: v3 managed account struct

---
+++ src/Struct/V3/ManagedAccount/Process.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Shopware\PayPalSDK\Struct\V3\ManagedAccount;
+
+class Process
+{
+
+}
---
+++ src/Struct/V3/ManagedAccount/ProcessCollection.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\PayPalSDK\Struct\V3\ManagedAccount;
+
+use Shopware\PayPalSDK\Struct\Collection;
+
+class ProcessesCollection extends Collection
+{
+
+}
